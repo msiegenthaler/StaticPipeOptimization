2682e07f0e983b8ea3f1a3a6b867759f6e6ab8c3
Mario Siegenthaler <msiegenthaler@gmx.net>
2012-04-14 14:43:55 +0200

    add operations for and, or

--- HBool.hs
+++ HBool.hs
@@ -4,7 +4,10 @@ module HBool (
     HTrue,
     HFalse,
     HBool,
-    HNegate
+    -- * Operations
+    HNegate,
+    HAnd,
+    HOr
 ) where
 
 data HTrue
@@ -14,6 +17,23 @@ class HBool b where hBool :: b -> Bool
 instance HBool HTrue where hBool _ = True
 instance HBool HFalse where hBool _ = False
 
+instance Show HTrue where show _ = "hTrue"
+instance Show HFalse where show _ = "hFalse"
+
+
 class HBool b => HNegate b result | b -> result
 instance HNegate HTrue HFalse
 instance HNegate HFalse HTrue
+
+class (HBool a, HBool b, HBool result) => HAnd a b result | a b -> result
+instance HAnd HTrue HTrue HTrue
+instance HAnd HFalse HTrue HFalse
+instance HAnd HTrue HFalse HFalse
+instance HAnd HFalse HFalse HFalse
+
+class (HBool a, HBool b, HBool result) => HOr a b result | a b -> result
+instance HOr HTrue HTrue HTrue
+instance HOr HFalse HTrue HTrue
+instance HOr HTrue HFalse HTrue
+instance HOr HFalse HFalse HFalse
+
