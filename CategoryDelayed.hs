5d2aa2656e91d0bb3c321b2d7083e7f5d49d9787
Mario Siegenthaler <msiegenthaler@gmx.net>
2012-05-15 20:47:26 +0200

    first version of category chain

--- CategoryDelayed.hs
+++ CategoryDelayed.hs
@@ -1,82 +1,53 @@
-{-# LANGUAGE GADTs, MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, FlexibleContexts, UndecidableInstances #-}
+{-# LANGUAGE GADTs, MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, FlexibleContexts, UndecidableInstances, ScopedTypeVariables #-}
 
 module CategoryDelayed (
 ) where
 
+import Prelude hiding (id)
 import Control.Category
 
 
-class Category cat => CategoryChain chain cat a b | chain -> cat a b
-
-
-data CategoryBox cat a b where
-    CategoryBox :: Category cat => cat a b -> CategoryBox cat a b
-instance Category cat => CategoryChain (CategoryBox cat a b) cat a b
+class Category cat => CategoryChain chain cat a b | chain -> cat a b where
+    applyChain :: chain -> cat a b
 
 data CCNil = CCNil
-instance Category cat => CategoryChain CCNil cat a b
+instance Category cat => CategoryChain CCNil cat a a where
+    applyChain _ = id
 instance Show CCNil where
     show _ = "CCNil"
 
 data CCCons cat a b c tail where
-    CCCons :: (Category cat, CategoryChain tail cat b c) =>
-                    cat a b -> tail -> CCCons cat a b c tail
+    CCCons :: (Category cat, CategoryChain tail cat b c) => cat a b -> tail -> CCCons cat a b c tail
 instance (Category cat) =>
-            CategoryChain (CCCons cat a b c tail) cat a c
-
-instance (Category cat, Show (cat a b), Show tail, CategoryChain tail cat a c, Show (cat a c)) =>
+            CategoryChain (CCCons cat a b c tail) cat a c where
+    applyChain (CCCons e t) = e >>> applyChain t
+instance (CategoryChain tail cat a c, Show tail, Show (cat a b)) =>
             Show (CCCons cat a b c tail) where
-    show (CCCons e t) = "CCons " ++ show e ++ " (" ++ show t ++ ")"
+    show (CCCons e t) = "CCCons " ++ show e ++ " (" ++ show t ++ ")"
+
+
+(*>>>) :: (Category cat, CategoryChain tail cat b c) => cat a b -> tail -> CCCons cat a b c tail
+(*>>>) = CCCons
+infixr 1 *>>>
 
 
 
-
-xxx :: (Category cat, CategoryChain chain cat a b) => chain -> String
-xxx _ = "ok"
-
-yyy :: (CategoryChain chain MyFuns a b) => chain -> String
-yyy _ = "ok"
-
-
-
-
-
-data MyFuns i o = AddOne
-                | AddTwo
-                | Ident deriving (Show, Eq)
+--- Example Category
+data MyFuns i o where
+    AddOne :: MyFuns Int Int
+    AddTwo :: MyFuns Int Int
+    AddX   :: Int -> MyFuns Int Int
+    Ident  :: MyFuns a a
 instance Category MyFuns where
     id = Ident
-    a . b = undefined
-
-
-
-
-
-{-
-
-data CatNil cat a = CatNil
-data CatCons cat a c t where
-    CatCons :: (Category cat, CategoryDelayed t cat b c) => cat a b -> t -> CatCons h a c t
-
-
-class Category cat => CategoryDelayed del cat a b
--- instance Category cat => CategoryDelayed (CatNil (cat a a) a) cat a a
-instance (Category cat1, Category cat0) => CategoryDelayed (CatNil (cat1 a1 a1) a1) cat0 a0 a0
-instance Category cat => CategoryDelayed (CatCons (cat a b) a c t) cat a c
-
-instance Show (CatNil cat a) where
-    show _ = "CatNil"
-instance Show (CatCons cat a b t) where
-    show _ = "CatCons"
-
-(>>>) :: (Category cat, CategoryDelayed t cat b c) => cat a b -> t -> CatCons h a c t
-(>>>) = CatCons
-
-
-myCat :: String -> String
-myCat i = i ++ "!"
-
-
-xxx :: (Category cat, CategoryDelayed del cat a a) => del -> String
-xxx _ = "ok"
--}
+    Ident . o = o
+    o . Ident = o
+    AddOne . AddOne = AddTwo
+    AddOne . AddTwo = AddX 3
+    AddTwo . AddOne = AddX 3
+    AddTwo . AddTwo = AddX 4
+instance Show (MyFuns a b) where
+    show Ident = "Ident"
+    show AddOne = "AddOne"
+    show AddTwo = "AddTwo"
+    show (AddX x) = "AddX " ++ show x
